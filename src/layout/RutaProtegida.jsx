c1848e6a2a4026fdf8a643c3e2fbd918028576bb
Majogobla <marcosjgodoyb@gmail.com>
2023-03-02 11:52:25 -0600

    useAuth import resolve

--- src/layout/RutaProtegida.jsx
+++ src/layout/RutaProtegida.jsx
@@ -1,7 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
-import useAuth from "../hook/useAuth.JSX";
+import useAuth from '../hook/useAuth.jsx';
 
 const RutaProtegida = () =>
 {
